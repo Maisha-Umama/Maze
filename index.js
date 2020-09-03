8cf0a403ad2e43c456fe0ab6bce94971b6bea286
Maisha Umama <umamamaisha@gmail.com>
2020-09-03 11:13:02 +0600

    maze generation complete

--- index.js
+++ index.js
@@ -111,12 +111,39 @@ const stepThroughCell = (row, column) => {
 
   stepThroughCell(startRow, startColumn);
 
-  horizontals.forEach(row=> {
-      row.forEach ((open)=> {
+  horizontals.forEach((row,rowIndex)=> {
+      row.forEach ((open,columnIndex) => {
           if(open){
               return;
           }
 
-          const wall =Bodies.rectangle();
+          const wall =Bodies.rectangle(
+              columnIndex * unitLength + unitLength / 2,
+              rowIndex  * unitLength + unitLength,
+            unitLength,
+            10,{
+                isStatic:true
+            }
+          );
+          World.add(world, wall);
+      });
+  });
+
+  verticals.forEach((row,rowIndex)=>{
+      row.forEach((open,columnIndex)=>{
+        if(open){
+            return;
+        }
+
+        const wall = Bodies.rectangle(
+            columnIndex * unitLength + unitLength,
+            rowIndex * unitLength + unitLength /2 ,
+            10,
+            unitLength,
+            {
+                isStatic:true
+            }
+        );
+        World.add (world,wall);
       });
   });
